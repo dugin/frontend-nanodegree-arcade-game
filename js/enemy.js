cb1c895b848d887f6c8ace2f5b6bc423fd0c1e73
Rodrigo Dugin <>
2018-03-31 17:45:55 -0300

    [refact] using class on enemy and player

--- js/enemy.js
+++ js/enemy.js
@@ -1,31 +1,26 @@
 // Enemies our player must avoid
 
-const Enemy = function(positionY) {
-  // Variables applied to each of our instances go here,
-  // we've provided one for you to get started
+class Enemy {
+  constructor(positionY) {
+    this.x = generateRandom(0, 5) * BLOCK.DISTANCE_HORIZONTAL;
+    this.y = getPositionFromMiddleOfBlock(positionY);
+    this.speed = generateRandom(20 - ENEMY.AMOUNT, 20) * 10;
 
-  this.x = generateRandom(0, 5) * BLOCK.DISTANCE_HORIZONTAL;
-  this.y = getPositionFromMiddleOfBlock(positionY);
-  this.speed = generateRandom(20 - ENEMY.AMOUNT, 20) * 10;
-  // The image/sprite for our enemies, this uses
-  // a helper we've provided to easily load images
-  this.sprite = "images/enemy-bug.png";
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-  const moveEnemy = this.x + this.speed * dt;
-  const initialPosition = -BLOCK.DISTANCE_HORIZONTAL;
-
-  if (isInRightBoundary(this.x, BLOCK.DISTANCE_HORIZONTAL)) {
-    this.x = moveEnemy;
-  } else {
-    this.x = initialPosition;
+    this.sprite = "images/enemy-bug.png";
   }
-};
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+  update(dt) {
+    const moveEnemy = this.x + this.speed * dt;
+    const initialPosition = -BLOCK.DISTANCE_HORIZONTAL;
+
+    if (isInRightBoundary(this.x, BLOCK.DISTANCE_HORIZONTAL)) {
+      this.x = moveEnemy;
+    } else {
+      this.x = initialPosition;
+    }
+  }
+
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+}
--- js/player.js
+++ js/player.js
@@ -1,90 +1,88 @@
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+class Player {
+  constructor() {
+    this._init();
 
-const Player = function() {
-  this.init();
-
-  this.sprite = "images/char-boy.png";
-};
-
-Player.prototype.init = function() {
-  this.x = 2 * BLOCK.DISTANCE_HORIZONTAL;
-  this.y = getPositionFromMiddleOfBlock(5);
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Player.prototype.update = function() {
-  // You should multiply any movement by the dt parameter
-  // which will ensure the game runs at the same speed for
-  // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Player.prototype.render = function() {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-Player.prototype.handleInput = function(allowedKeys) {
-  switch (allowedKeys) {
-    case PLAYER_MOVEMENT.LEFT:
-      this.moveLeft();
-      break;
-    case PLAYER_MOVEMENT.RIGHT:
-      this.moveRight();
-      break;
-    case PLAYER_MOVEMENT.UP:
-      this.moveUp();
-      break;
-    case PLAYER_MOVEMENT.DOWN:
-      this.moveDown();
-      break;
-
-    default:
-      break;
+    this.sprite = "images/char-boy.png";
   }
-};
-
-Player.prototype.moveLeft = function() {
-  const leftBoundary = 0;
-
-  const isInLeftBoundary = this.x > leftBoundary;
-  if (isInLeftBoundary) {
-    this.x = this.x - BLOCK.DISTANCE_HORIZONTAL;
-  }
-};
-
-Player.prototype.moveRight = function() {
-  console.log(isInRightBoundary(this.x));
-  if (isInRightBoundary(this.x)) {
-    this.x = this.x + BLOCK.DISTANCE_HORIZONTAL;
-  }
-};
-
-Player.prototype.moveUp = function() {
-  const topBoundary = BLOCK.MIDDLE_OF_BLOCK_VERTICAL;
-
-  const isInTopBoundary = this.y >= topBoundary;
-
-  if (isInTopBoundary) {
-    this.y = this.y - BLOCK.DISTANCE_VERTICAL;
+  // Update the enemy's position, required method for game
+  // Parameter: dt, a time delta between ticks
+  update() {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
   }
 
-  if (this.y < BLOCK.DISTANCE_VERTICAL + topBoundary) {
-    setTimeout(() => {
-      this.init();
-    }, 500);
+  // Draw the enemy on the screen, required method for game
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
-};
 
-Player.prototype.moveDown = function() {
-  const bottomBoundary =
-    BLOCK.DISTANCE_VERTICAL * (numCols - 1) + BLOCK.MIDDLE_OF_BLOCK_VERTICAL;
+  handleInput(allowedKeys) {
+    switch (allowedKeys) {
+      case PLAYER_MOVEMENT.LEFT:
+        this._moveLeft();
+        break;
+      case PLAYER_MOVEMENT.RIGHT:
+        this._moveRight();
+        break;
+      case PLAYER_MOVEMENT.UP:
+        this._moveUp();
+        break;
+      case PLAYER_MOVEMENT.DOWN:
+        this._moveDown();
+        break;
 
-  const isInBottomBoundary = this.y < bottomBoundary;
-
-  if (isInBottomBoundary) {
-    this.y = this.y + BLOCK.DISTANCE_VERTICAL;
+      default:
+        break;
+    }
   }
-};
+
+  _init() {
+    this.x = 2 * BLOCK.DISTANCE_HORIZONTAL;
+    this.y = getPositionFromMiddleOfBlock(5);
+  }
+
+  _moveLeft() {
+    const leftBoundary = 0;
+
+    const isInLeftBoundary = this.x > leftBoundary;
+    if (isInLeftBoundary) {
+      this.x = this.x - BLOCK.DISTANCE_HORIZONTAL;
+    }
+  }
+
+  _moveRight() {
+    if (isInRightBoundary(this.x)) {
+      this.x = this.x + BLOCK.DISTANCE_HORIZONTAL;
+    }
+  }
+
+  _moveUp() {
+    const topBoundary = BLOCK.MIDDLE_OF_BLOCK_VERTICAL;
+
+    const isInTopBoundary = this.y >= topBoundary;
+
+    const isInTheWater = this.y < BLOCK.DISTANCE_VERTICAL + topBoundary;
+
+    if (isInTopBoundary) {
+      this.y = this.y - BLOCK.DISTANCE_VERTICAL;
+    }
+
+    if (isInTheWater) {
+      setTimeout(() => {
+        this._init();
+      }, 300);
+    }
+  }
+
+  _moveDown() {
+    const bottomBoundary =
+      BLOCK.DISTANCE_VERTICAL * (numCols - 1) + BLOCK.MIDDLE_OF_BLOCK_VERTICAL;
+
+    const isInBottomBoundary = this.y < bottomBoundary;
+
+    if (isInBottomBoundary) {
+      this.y = this.y + BLOCK.DISTANCE_VERTICAL;
+    }
+  }
+}
